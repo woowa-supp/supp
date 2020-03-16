de45d61819ccb49d323d3b7ebb5c1d5e6afb2ada
kouz <lastpuzly@gmail.com>
2020-03-16 17:39:24 +0900

    feat: github 로그인 기능 구현

---
+++ src/main/java/com/woowa/supp/config/auth/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.woowa.supp.config.auth;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import com.woowa.supp.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	private final CustomOAuth2UserService customOAuth2UserService;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.csrf().disable()
+			.headers().frameOptions().disable()
+			.and()
+			.authorizeRequests()
+			.antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
+			.antMatchers("/api/v1/**").hasRole(Role.USER.name())
+			.anyRequest().authenticated()
+			.and()
+			.logout()
+			.logoutSuccessUrl("/")
+			.and()
+			.oauth2Login()
+			.userInfoEndpoint()
+			.userService(customOAuth2UserService);
+	}
+}
--- src/main/java/com/woowa/supp/web/IndexController.java
+++ src/main/java/com/woowa/supp/web/IndexController.java
@@ -4,13 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.woowa.supp.config.auth.LoginUser;
+import com.woowa.supp.config.auth.dto.SessionUser;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 	@GetMapping("/")
-	public String index() {
+	public String index(Model model, @LoginUser SessionUser user) {
+		if (user != null) {
+			model.addAttribute("userName", user.getName());
+		}
 		return "index";
 	}
 
