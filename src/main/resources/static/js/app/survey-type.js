48330a2ad83274ce50f9566414725629a7eaa39a
kouz <lastpuzly@gmail.com>
2020-04-27 20:39:36 +0900

    feat: index page background 추가

--- src/main/resources/static/js/app/survey-type.js
+++ src/main/resources/static/js/app/survey-type.js
@@ -152,3 +152,28 @@ var main = {
 };
 
 main.init();
+
+//
+
+(function($) {
+
+    var form = $("#signup-form");
+    form.steps({
+        headerTag: "h3",
+        bodyTag: "fieldset",
+        transitionEffect: "fade",
+        labels: {
+            previous : 'Previous',
+            next : 'Next',
+            finish : 'Submit',
+            current : ''
+        },
+        titleTemplate : '<div class="title"><span class="title-text">#title#</span><span class="title-number">0#index#</span></div>',
+        onFinished: function (event, currentIndex)
+        {
+            alert('Sumited');
+        }
+    });
+
+
+})(jQuery);
