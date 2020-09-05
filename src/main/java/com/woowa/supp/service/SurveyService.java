57f0a545189526d488a1ebae7f7c334018f0a56e
jnsorn <jnsorn@gmail.com>
2020-09-05 20:13:17 +0900

    docs : exception에 대한 문서화

--- src/main/java/com/woowa/supp/service/SurveyService.java
+++ src/main/java/com/woowa/supp/service/SurveyService.java
@@ -13,61 +13,64 @@ import com.woowa.supp.domain.surveyee.SurveyeeRepository;
 import com.woowa.supp.web.dto.DeveloperTypeSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 
+/**
+ * @throws UserNotFoundException 우아한테크코스 사이트에서 페어의 결과 아이콘 클릭 시 페어(사용자)가 존재하지 않을 경우, SUPP 페이지에서 로그인 한 사용자가 존재지 않을 경우
+ */
 @RequiredArgsConstructor
 @Service
 public class SurveyService {
 
-	private final SurveyeeRepository surveyeeRepository;
+    private final SurveyeeRepository surveyeeRepository;
 
-	@Transactional
-	public Long saveOrUpdateType(DeveloperTypeSaveRequestDto requestDto, SessionUser user) {
-		Optional<Surveyee> persist = surveyeeRepository.findByLogin(user.getLogin());
-		if (persist.isPresent()) {
-			return updateType(requestDto, persist.get());
-		}
-		return saveType(requestDto, user);
-	}
+    @Transactional
+    public Long saveOrUpdateType(DeveloperTypeSaveRequestDto requestDto, SessionUser user) {
+        Optional<Surveyee> persist = findOptionalByUser(user);
+        if (persist.isPresent()) {
+            return updateType(requestDto, persist.get());
+        }
+        return saveType(requestDto, user);
+    }
 
-	private Long updateType(DeveloperTypeSaveRequestDto requestDto, Surveyee persist) {
-		persist.updateTypeBy(requestDto.toEntity());
-		return persist.getId();
-	}
+    @Transactional(readOnly = true)
+    public Optional<Surveyee> findOptionalByUser(SessionUser user) {
+        return surveyeeRepository.findByLogin(user.getLogin());
+    }
 
-	private Long saveType(DeveloperTypeSaveRequestDto requestDto,
-			@LoginUser SessionUser sessionUser) {
-		Surveyee surveyee = Surveyee.builder()
-				.login(sessionUser.getLogin())
-				.avatar(sessionUser.getAvatar())
-				.developerType(requestDto.toEntity())
-				.build();
+    private Long updateType(DeveloperTypeSaveRequestDto requestDto, Surveyee persist) {
+        persist.updateTypeBy(requestDto.toEntity());
+        return persist.getId();
+    }
 
-		return surveyeeRepository.save(surveyee).getId();
-	}
+    private Long saveType(DeveloperTypeSaveRequestDto requestDto,
+            @LoginUser SessionUser sessionUser) {
+        Surveyee surveyee = Surveyee.builder()
+                .login(sessionUser.getLogin())
+                .avatar(sessionUser.getAvatar())
+                .developerType(requestDto.toEntity())
+                .build();
 
-	@Transactional
-	public Long saveStyle(Map<String, Object> styles, @LoginUser SessionUser sessionUser) {
-		Optional<Surveyee> persist = surveyeeRepository.findByLogin(sessionUser.getLogin());
-		if (persist.isPresent()) {
-			persist.get().updateStylesBy(styles);
-			return persist.get().getId();
-		}
-		throw new UserNotFoundException(sessionUser.getLogin());
-	}
+        return surveyeeRepository.save(surveyee).getId();
+    }
 
-	@Transactional(readOnly = true)
-	public Optional<Surveyee> findOptionalByUser(SessionUser user) {
-		return surveyeeRepository.findByLogin(user.getLogin());
-	}
+    @Transactional
+    public Long saveStyle(Map<String, Object> styles, @LoginUser SessionUser sessionUser) {
+        Optional<Surveyee> persist = findOptionalByUser(sessionUser);
+        if (persist.isPresent()) {
+            persist.get().updateStylesBy(styles);
+            return persist.get().getId();
+        }
+        throw new UserNotFoundException(sessionUser.getLogin());
+    }
 
-	@Transactional(readOnly = true)
-	public Surveyee findByUser(SessionUser user) {
-		return findByLogin(user.getLogin());
-	}
+    @Transactional(readOnly = true)
+    public Surveyee findByUser(SessionUser user) {
+        return findByLogin(user.getLogin());
+    }
 
-	@Transactional(readOnly = true)
-	public Surveyee findByLogin(String login) {
-		return surveyeeRepository.findByLogin(login)
-				.orElseThrow(() -> new UserNotFoundException(login));
-	}
+    @Transactional(readOnly = true)
+    public Surveyee findByLogin(String login) {
+        return surveyeeRepository.findByLogin(login)
+                .orElseThrow(() -> new UserNotFoundException(login));
+    }
 }
 
