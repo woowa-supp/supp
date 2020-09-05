33c5280a4047ccae2d27d44546bf04b9536430ae
jnsorn <jnsorn@gmail.com>
2020-09-05 20:24:38 +0900

    refactor : style과 type에 대한 예외 처리

--- src/main/java/com/woowa/supp/domain/surveyee/SurveyeeRepository.java
+++ src/main/java/com/woowa/supp/domain/surveyee/SurveyeeRepository.java
@@ -5,6 +5,5 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SurveyeeRepository extends JpaRepository<Surveyee, Long> {
-
 	Optional<Surveyee> findByLogin(String login);
 }
--- src/main/java/com/woowa/supp/domain/surveyee/style/BreaktimeStyle.java
+++ src/main/java/com/woowa/supp/domain/surveyee/style/BreaktimeStyle.java
@@ -3,20 +3,20 @@ package com.woowa.supp.domain.surveyee.style;
 import java.util.Arrays;
 
 public enum BreaktimeStyle {
-	NO_BREAK("저는 진짜 힘들때까진 계속 코딩 하는 편이에요."),
-	SCHEDULED_BREAK("저는 일정 시간마다 쉬어야 합니다."),
-	DONT_CARE("저는 뭐... 상관 없습니다.");
+    NO_BREAK("저는 진짜 힘들때까진 계속 코딩 하는 편이에요."),
+    SCHEDULED_BREAK("저는 일정 시간마다 쉬어야 합니다."),
+    DONT_CARE("저는 뭐... 상관 없습니다.");
 
-	private final String title;
+    private final String title;
 
-	BreaktimeStyle(String title) {
-		this.title = title;
-	}
+    BreaktimeStyle(String title) {
+        this.title = title;
+    }
 
-	public static BreaktimeStyle of(String title) {
-		return Arrays.stream(BreaktimeStyle.values())
-		             .filter(value -> value.title.equals(title))
-		             .findFirst()
-		             .orElseThrow(() -> new IllegalArgumentException(title + ": 적절하지 않은 답변 이름 입니다."));
-	}
+    public static BreaktimeStyle of(String title) {
+        return Arrays.stream(BreaktimeStyle.values())
+                .filter(value -> value.title.equals(title))
+                .findFirst()
+                .orElseThrow(() -> new InvalidStyleException(title));
+    }
 }
